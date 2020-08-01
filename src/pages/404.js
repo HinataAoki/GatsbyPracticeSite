38e88f512b8f3b4808fc1ad6379326b49c10911f
HinataAoki <sunreverse.0311.blue@gmail.com>
2020-08-02 04:40:40 +0900

    5-4章終了

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import React from "react"
-import Layout from "../components/layout"
 
-export default () => (
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default ({location}) => (
   <Layout>
-    <h1 style={{ padding: "20vh 0", textAlign: "center"}}>
+    <SEO pagetitle="ページが見つかりません" pagepath={location.pathname} />
+    <h1 style={{ padding: `20vh 0`, textAlign: `center`}}>
       お探しのページが見つかりませんでした。
     </h1>
   </Layout>
