887f96100202a76ba2da276c58b030fcaa2b28b0
HinataAoki <sunreverse.0311.blue@gmail.com>
2020-08-03 21:49:02 +0900

    8章終了

--- src/components/seo.js
+++ src/components/seo.js
@@ -21,11 +21,13 @@ export default props => {
   const description = props.pagedesc || data.site.siteMetadata.description
   const url = props.pagepath ? `${data.site.siteMetadata.siteUrl}${props.pagepath}` : data.site.siteMetadata.siteUrl
 
-  const imgurl = props.pageimg ? `${data.site.siteMetadata.siteUrl}${props.pageimg}` : `${data.site.siteMetadata.siteUrl}/thumb.jpg`
+  const imgurl = props.pageimg
+    ? `${data.site.siteMetadata.siteUrl}${props.pageimg}`
+    : props.blogimg || `${data.site.siteMetadata.siteUrl}/thumb.jpg`
   const imgw = props.pageimgw || 1280
   const imgh = props.pageimgh || 640
 
-  return(
+  return (
     <Helmet>
       <html lang={data.site.siteMetadata.lang} />
       <title>{title}</title>
